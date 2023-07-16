ac5d0c28d49a9dd63fbf4d76c176495f483e8ff9
jos-gabriel <jos.gabrie@gmail.com>
2023-07-15 23:41:58 -0600

    InterfazLoggin

---
+++ src/main/java/main/main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+import views.LogginView;
+
+/**
+ *
+ * @author jos_g
+ */
+public class main {
+    public static void main(String[] args) {
+        //Instancia d login 
+        LogginView loggin = new LogginView();
+        loggin.setVisible(true);
+        
+        
+        
+    }
+    
+}
